43376003d55609d96c22f798c7582b73b9862626
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-08-08 00:58:26 +0300

    add llibraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+// var debounce = require('lodash.debounce');
+
+import debounce from 'lodash.debounce';
+
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+
+fetch('https://restcountries.com/v2/all?fields=name.official,capital,population,flags.svg,languages').then(response => response.json).then(data => console.log(data)).catch(error => console.log(error));
+
+
+
+
+
+// https://restcountries.com/v2/all?fields=name.official,capital,population,flags.svg,languages
+
+// https://restcountries.com/v3.1/all
